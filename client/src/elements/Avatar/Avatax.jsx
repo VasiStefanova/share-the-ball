ef333a2539cafad131817e243615d8827885959b
Konstantin Gushterov <konstantin.gushterov@gmail.com>
2021-11-17 11:56:33 +0200

    #9 Partial design of avatar

---
+++ client/src/elements/Avatar/Avatax.jsx
@@ -0,0 +1,11 @@
+import './Avatar.css';
+
+const Avatar = () => {
+  return (
+    <div className='avatarBox'>
+      <h3 style={{ color: 'white', margin: '0px' }}>Avatar</h3>
+    </div>
+  );
+};
+
+export default Avatar;
