7b963631b50eb3b81cf30a0ea5e8d99a10074027
Vasilka Stefanova <vasilka.stefanova.a32@learn.telerikacademy.com>
2021-11-17 20:53:39 +0200

    NBA team logos urls added

--- client/src/common/constants.js
+++ client/src/common/constants.js
@@ -2,3 +2,70 @@ export const USERS_URL = 'http://localhost:5000/users';
 export const POSTS_URL = 'http://localhost:5000/posts';
 export const COMMENTS_URL = 'http://localhost:5000/comments';
 export const FEED_URL = 'http://localhost:5000/feed';
+export const NBA_GENERAL_URL = 'http://data.nba.net/10s/prod/v1/today.json'; // general//
+export const NBA_STANDINGS_URL = 'http://data.nba.net/10s/prod/v1/current/standings_conference.json'; // teams-conference
+
+// export const NBA_NEWS_URL = 'http://data.nba.net/10s//prod/v1/2021/schedule.json'; // leagueSchedule
+
+export const TEAM_LOGOS_URL = [
+  {
+    'teamId': '1610612764',
+    'logoURL': 'https://upload.wikimedia.org/wikipedia/en/thumb/0/02/Washington_Wizards_logo.svg/1200px-Washington_Wizards_logo.svg.png'
+  },
+  {
+    'teamId': '1610612741',
+    'logoURL': 'https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Chicago_Bulls_logo.svg/1200px-Chicago_Bulls_logo.svg.png'
+  },
+  {
+    'teamId': '1610612751',
+    'logoURL': 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Brooklyn_Nets_newlogo.svg/1200px-Brooklyn_Nets_newlogo.svg.png'
+  },
+  {
+    'teamId': '1610612748',
+    'logoURL': 'https://upload.wikimedia.org/wikipedia/en/thumb/f/fb/Miami_Heat_logo.svg/1200px-Miami_Heat_logo.svg.png'
+  },
+  {
+    'teamId': '1610612739',
+    'logoURL': 'https://logos-world.net/wp-content/uploads/2020/05/Cleveland-Cavaliers-logo.png'
+  },
+  {
+    'teamId': '1610612752',
+    'logoURL': 'https://upload.wikimedia.org/wikipedia/en/thumb/2/25/New_York_Knicks_logo.svg/1200px-New_York_Knicks_logo.svg.png'
+  },
+  {
+    'teamId': '1610612766',
+    'logoURL': 'https://upload.wikimedia.org/wikipedia/en/thumb/c/c4/Charlotte_Hornets_%282014%29.svg/1200px-Charlotte_Hornets_%282014%29.svg.png'
+  },
+  {
+    'teamId': '1610612755',
+    'logoURL': 'https://upload.wikimedia.org/wikipedia/en/thumb/0/0e/Philadelphia_76ers_logo.svg/1200px-Philadelphia_76ers_logo.svg.png'
+  },
+  {
+    'teamId': '1610612738',
+    'logoURL': 'https://upload.wikimedia.org/wikipedia/en/thumb/8/8f/Boston_Celtics.svg/1200px-Boston_Celtics.svg.png'
+  },
+  {
+    'teamId': '1610612761',
+    'logoURL': 'https://upload.wikimedia.org/wikipedia/en/thumb/3/36/Toronto_Raptors_logo.svg/1200px-Toronto_Raptors_logo.svg.png'
+  },
+  {
+    'teamId': '1610612749',
+    'logoURL': 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4a/Milwaukee_Bucks_logo.svg/1200px-Milwaukee_Bucks_logo.svg.png'
+  },
+  {
+    'teamId': '1610612737',
+    'logoURL': 'https://upload.wikimedia.org/wikipedia/en/thumb/2/24/Atlanta_Hawks_logo.svg/1200px-Atlanta_Hawks_logo.svg.png'
+  },
+  {
+    'teamId': '1610612754',
+    'logoURL': 'https://upload.wikimedia.org/wikipedia/en/thumb/1/1b/Indiana_Pacers.svg/1200px-Indiana_Pacers.svg.png'
+  },
+  {
+    'teamId': '1610612765',
+    'logoURL': 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Pistons_logo17.svg/1200px-Pistons_logo17.svg.png'
+  },
+  {
+    'teamId': '1610612753',
+    'logoURL': 'https://upload.wikimedia.org/wikipedia/en/thumb/1/10/Orlando_Magic_logo.svg/1200px-Orlando_Magic_logo.svg.png'
+  }
+];
