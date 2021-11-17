0854db7396fb7c1fb47de492d2326d0e783a79be
Martin Petrov <martinpetrov9412@gmail.com>
2021-11-17 12:13:11 +0200

    Added bootstrap import to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
