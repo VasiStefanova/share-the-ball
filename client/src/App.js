bd09cd14019276b561288be4eebdc45c72892a9c
Konstantin Gushterov <konstantin.gushterov@gmail.com>
2021-11-17 09:51:49 +0200

    Implemented router

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
+import React from 'react';
+import {BrowserRouter, Routes} from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
 
 // eslint-disable-next-line require-jsdoc
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          {/* <Route exact path="/"><Navigate to="/home" /></Route> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
