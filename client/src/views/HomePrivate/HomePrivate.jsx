a7f86e71a637d1e4298e9f0997d04b7c35a9d276
Martin Petrov <martinpetrov9412@gmail.com>
2021-11-20 05:12:27 +0200

    #21 Implemented posts for home private view

--- client/src/views/HomePrivate/HomePrivate.jsx
+++ client/src/views/HomePrivate/HomePrivate.jsx
@@ -1,8 +1,24 @@
 import './HomePrivate.css';
 import NBAStandingsFeed from '../../components/NBAStandingsFeed/NBAStandingsFeed';
 import CreatePost from '../../components/CreatePost/CreatePost';
+import { useContext, useEffect, useState } from 'react';
+import { getAllPostsRequest } from '../../services/feed/get-all-posts-request';
+import SinglePost from '../../components/SinglePost/SinglePost';
+import AppContext from '../../context/AppContext';
 
 const HomePrivate = () => {
+  const { createdPost } = useContext(AppContext);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(async () => {
+    try {
+      const allPosts = await getAllPostsRequest();
+      setPosts(allPosts);
+    } catch (error) {
+      console.error(error);
+    }
+  }, [createdPost]);
+
   return (
     <div className='homePrivateContainer'>
       <div className='homePrivateLeft'>
@@ -10,6 +26,13 @@ const HomePrivate = () => {
       </div>
       <div className='homePrivateCenter'>
         <CreatePost />
+        <div className="privatePosts">
+          {posts.map(post => (
+            <div className="singlePost" key={post.id}>
+              <SinglePost post={post} />
+            </div>
+          ))}
+        </div>
       </div>
       <div className='homePrivateRight'>
         <h3>Right</h3>
