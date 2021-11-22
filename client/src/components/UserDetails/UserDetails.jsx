d4f083e0c6de03f4f489b61797edab748734c8db
Vasilka Stefanova <vasilka.stefanova.a32@learn.telerikacademy.com>
2021-11-22 17:22:32 +0200

    #29 btn cases added to user's profile view

--- client/src/components/UserDetails/UserDetails.jsx
+++ client/src/components/UserDetails/UserDetails.jsx
@@ -1,17 +1,48 @@
 import PropTypes from 'prop-types';
 import Figure from 'react-bootstrap/Figure';
 import Avatar from '../../elements/Avatar/Avatax';
+import Button from 'react-bootstrap/Button';
 import './UserDetails.css';
+import { useContext } from 'react';
+import AppContext from '../../context/AppContext';
 
 
 const UserDetails = ({ userInfo }) => {
+  const { user } = useContext(AppContext);
 
+  const friendConnection = user.friends.find(friend => friend.id === userInfo.id);
+  let btnProps = {};
+
+  switch (friendConnection?.friendshipStatus) {
+  case 1:
+    btnProps = {
+      label: 'pending',
+      disabled: true,
+      iconClassName: 'bi bi-send-exclamation-fill'
+    };
+    break;
+  case 2:
+    btnProps = {
+      label: 'remove teammate',
+      iconClassName: 'bi bi-person-dash-fill',
+      handleClick: () => console.log('removed!')
+    };
+
+    break;
+  default:
+    btnProps = {
+      label: 'Add teammate',
+      iconClassName: 'bi bi-person-plus-fill',
+      handleClick: () => console.log('added!')
+    };
+  }
 
   return (
     <Figure>
       <Avatar
         user={userInfo}
         style={{
+          'margin-top': '3vh',
           'max-height': '30vh',
           'max-width': '40vh',
           'border-radius': '30vh'
@@ -22,6 +53,10 @@ const UserDetails = ({ userInfo }) => {
         <p className='user-text'>email: {userInfo.email}</p>
         <p className='user-text'>last updated on: {new Date(userInfo.lastUpdated).toLocaleDateString('en-UK')}</p>
       </Figure.Caption>
+      <Button variant='dark' disabled={btnProps.disabled} onClick={btnProps.handleClick}>
+        <i className={btnProps.iconClassName} />
+        {btnProps.label}
+      </Button>
     </Figure>
   );
 };
