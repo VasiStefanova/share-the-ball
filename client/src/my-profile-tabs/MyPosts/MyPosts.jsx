4608db5f3d51f5cc7e653ae7e5e788cc39724200
Konstantin Gushterov <konstantin.gushterov@gmail.com>
2021-11-22 11:24:50 +0200

    #25 Partial implementation of my posts tab

--- client/src/my-profile-tabs/MyPosts/MyPosts.jsx
+++ client/src/my-profile-tabs/MyPosts/MyPosts.jsx
@@ -1,8 +1,30 @@
 import './MyPosts.css';
+import { useContext, useEffect, useState } from 'react';
+import AppContext from '../../context/AppContext';
+import { getPostsByUserIdRequest } from '../../services/posts/get-posts-by-user-id-request';
+import SinglePost from '../../components/SinglePost/SinglePost';
+import CreatePost from '../../components/CreatePost/CreatePost';
 
 const MyPosts = () => {
+  const { user } = useContext(AppContext);
+  const [myPosts, setMyPosts] = useState([]);
+
+  useEffect(async () => {
+    const posts = await getPostsByUserIdRequest(user.id);
+    setMyPosts(posts);
+  }, []);
+
   return (
-    <h2>My Posts</h2>
+    <>
+      <CreatePost />
+      <div className="private-posts">
+        {myPosts.map(post => (
+          <div className="single-post" key={post.id}>
+            <SinglePost post={post} />
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
