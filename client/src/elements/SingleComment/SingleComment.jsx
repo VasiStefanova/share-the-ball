26652f0c7382a4d7c3f18c2dad7ffc72f613c4fa
Martin Petrov <martinpetrov9412@gmail.com>
2021-11-24 14:26:42 +0200

    #31 Implemented comment post functionality

--- client/src/elements/SingleComment/SingleComment.jsx
+++ client/src/elements/SingleComment/SingleComment.jsx
@@ -1,1 +1,37 @@
+/* eslint-disable react/prop-types */
 import './SingleComment.css';
+import Button from 'react-bootstrap/Button';
+import Avatar from '../../elements/Avatar/Avatax';
+
+
+const SingleComment = ({ comment }) => {
+
+  return (
+    <>
+      <div className='comment-container'>
+        <div className='author-details theme-text-style'>
+          <Avatar user={comment.author} />
+          <h6 className='comment-author-username'>{comment.author.username}</h6>
+        </div>
+        <div className="comment-content">
+          <div className="comment-text theme-text-style">
+            <h5>
+              {comment.content}
+            </h5>
+          </div>
+          {comment.picture &&
+            <div className="comment-image-box">
+              <img src={`http://localhost:5000/${comment.picture}`} className='comment-image' />
+            </div>}
+        </div>
+        <div className="comment-buttons">
+          <Button variant="outline-dark" style={{ border: 'none' }}>Like</Button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+
+export default SingleComment;
+
