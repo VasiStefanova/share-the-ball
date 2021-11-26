fe87f999dc921013b07b16232e3d2ba32b593fd8
Martin Petrov <martinpetrov9412@gmail.com>
2021-11-26 14:13:28 +0200

    Minor bug fix to MyTeammates

--- client/src/my-profile-tabs/MyTeammates/MyTeammates.jsx
+++ client/src/my-profile-tabs/MyTeammates/MyTeammates.jsx
@@ -3,34 +3,33 @@ import './MyTeammates.css';
 import { useContext, useEffect, useState } from 'react';
 import FoundTeammates from '../../components/FoundTeammates/FoundTeammates';
 import SearchTeammates from '../../components/SearchTeammates/SearchTeammates';
-import getUsersRequest from '../../services/users/get-users-request';
 import AppContext from '../../context/AppContext';
 import getUserDetailsRequest from '../../services/users/get-user-details-request';
 
 const MyTeammates = ({ mountedOn = '' }) => {
   const { user, setUser, toggleFriendship } = useContext(AppContext);
   const [teammates, setTeammates] = useState([]);
+
   // I use userInfo, not just user to avoid react-state-update-warning upon logging out
   const [userInfo, setUserInfo] = useState(user);
 
   useEffect(() => {
-    getUserDetailsRequest(user.id)
+    if (!userInfo.id) return;
+
+    getUserDetailsRequest(userInfo.id)
       .then(userDetails => {
-        setUserInfo(userDetails);
         setUser(userDetails);
+        setUserInfo(userDetails);
+        localStorage.setItem('user', JSON.stringify(userDetails));
       })
       .catch(console.error);
   }, [toggleFriendship]);
 
   useEffect(() => {
-    getUsersRequest()
-      .then(allUsers => {
-        return allUsers
-          .filter(({ id: userId }) => userInfo.friends
-            .some(({ id: teammateId }) => userId === teammateId));
-      })
-      .then(teammatesList => setTeammates(teammatesList))
-      .catch(console.error);
+    if (!userInfo.friends) return;
+
+    setTeammates(userInfo.friends
+      .filter(friend => friend.friendshipStatus === 2));
   }, [userInfo]);
 
   return !teammates.length ?
