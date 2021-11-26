be1c23c1cf0c71eda7740415c4a9733d69d63f8d
Vasilka Stefanova <vasilka.stefanova.a32@learn.telerikacademy.com>
2021-11-26 17:47:24 +0200

    added friendship context for triggering re-render

--- client/src/my-profile-tabs/UserList/UserList.jsx
+++ client/src/my-profile-tabs/UserList/UserList.jsx
@@ -1,17 +1,19 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import FoundTeammates from '../../components/FoundTeammates/FoundTeammates';
 import SearchTeammates from '../../components/SearchTeammates/SearchTeammates';
 import getUsersRequest from '../../services/users/get-users-request';
 import './UserList.css';
+import AppContext from '../../context/AppContext';
 
 const UserList = () => {
 
   const [users, setUsers] = useState([]);
+  const { toggleFriendship } = useContext(AppContext);
 
   useEffect(() => {
     getUsersRequest()
       .then(allUsers => setUsers(allUsers));
-  }, []);
+  }, [toggleFriendship]);
 
   return !users.length ?
     <SearchTeammates setTeammates={setUsers} /> :
