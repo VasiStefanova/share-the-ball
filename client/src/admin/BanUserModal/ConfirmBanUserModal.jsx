16d606472b506dbeb3251279543dc7edca764148
Vasilka Stefanova <vasilka.stefanova.a32@learn.telerikacademy.com>
2021-11-27 18:49:52 +0200

    #26 completed functionalities

--- client/src/admin/BanUserModal/ConfirmBanUserModal.jsx
+++ client/src/admin/BanUserModal/ConfirmBanUserModal.jsx
@@ -26,39 +26,44 @@ const ConfirmBanUser = ({ userId, username, show, setShow }) => {
     });
   };
 
-  const banUser = (event) => {
+  const handleSubmit = (event) => {
+    const formEvent = event.currentTarget;
     event.preventDefault();
     event.stopPropagation();
-    setValid(!valid);
-    banUserRequest(userId, form)
-      .then(response => {
-        console.log(response);
-        setShow(false);
-        setToggleFriendship(!toggleFriendship);
-      })
-      .catch(err => console.error(err));
+    setValid(true);
+
+    if (formEvent.checkValidity()) {
+      setValid(false);
+      handleClose();
+      banUserRequest(userId, form)
+        .then(response => {
+          console.log(response);
+          handleClose();
+          setToggleFriendship(!toggleFriendship);
+        })
+        .catch(err => console.error(err));
+    }
   };
 
   return (
     <>
       <Modal className='modal-warning' show={show} onHide={handleClose} animation backdrop>
         <Modal.Header closeButton>
-          <Modal.Title>{`Are you sure you want to ban ${username}`}</Modal.Title>
+          <Modal.Title>{`Are you sure you want to ban ${username}?`}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form onSubmit={banUser} validated={valid} noValidate autoComplete="off">
+          <Form onSubmit={handleSubmit} validated={valid} noValidate autoComplete="off">
             <Form.Group className="mb-3" controlId="formBasicUsername">
               <Form.Label>Description</Form.Label>
               <Form.Control
                 type="text"
                 placeholder="Enter reason"
                 required
-                minLength={5}
-                maxLength={50}
+                minLength={4}
                 onChange={e => setField('reason', e.target.value)}
               />
               <Form.Control.Feedback type='invalid'>
-                Invalid reason! Reason must be at least 4 charcters long!
+                Invalid reason! Reason must be at least 4 characters long!
               </Form.Control.Feedback>
             </Form.Group>
 
@@ -71,19 +76,20 @@ const ConfirmBanUser = ({ userId, username, show, setShow }) => {
                 onChange={e => setField('period', Date.parse(e.target.value))}
               />
               <Form.Control.Feedback type='invalid'>
-                Invalid description! Description must be between 5-50 charcters long!
+                You must specify ban date!
               </Form.Control.Feedback>
             </Form.Group>
+            <Modal.Footer>
+              <Button variant="outline-dark" onClick={handleClose}>
+                No
+              </Button>
+              <Button type="submit" variant="outline-dark">
+                Ban
+              </Button>
+            </Modal.Footer>
           </Form>
         </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            No
-          </Button>
-          <Button variant="primary" onClick={banUser}>
-            Ban
-          </Button>
-        </Modal.Footer>
+
       </Modal>
     </>
   );
---
+++ client/src/admin/DeleteUserPost/DeleteUserPost.jsx
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import { deletePostRequest } from '../../services/posts/delete-post-request';
+import './DeleteUserPost.css';
+
+const DeleteUserPost = ({ postId, setRender }) => {
+
+  const deletePost = () => {
+
+    deletePostRequest(postId)
+      .then(response => {
+        setRender({});
+        console.log(response);
+      })
+      .catch(err => console.error(err));
+  };
+
+  return (
+    <Button
+      className='admin-delete-post-btn'
+      variant='outline-dark'
+      onClick={deletePost}
+    >
+      <i className="bi bi-trash-fill" />
+    </Button>
+  );
+};
+
+DeleteUserPost.propTypes = {
+  postId: PropTypes.array,
+  setRender: PropTypes.func
+};
+export default DeleteUserPost;
