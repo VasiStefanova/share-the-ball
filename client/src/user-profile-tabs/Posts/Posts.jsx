9a790bc64a5e5f0de5da82b55a622ddd89bf5874
Vasilka Stefanova <vasilka.stefanova.a32@learn.telerikacademy.com>
2021-11-27 19:20:40 +0200

    #28 made buttons to be seen only by admins

--- client/src/user-profile-tabs/Posts/Posts.jsx
+++ client/src/user-profile-tabs/Posts/Posts.jsx
@@ -1,14 +1,15 @@
 import PropTypes from 'prop-types';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import DeleteUserPost from '../../admin/DeleteUserPost/DeleteUserPost';
 import UpdateUserPost from '../../admin/UpdateUserPost/UpdateUserPost';
 import CreatePost from '../../components/CreatePost/CreatePost';
 import SinglePost from '../../components/SinglePost/SinglePost';
 import { getPostsByUserIdRequest } from '../../services/posts/get-posts-by-user-id-request';
+import AppContext from '../../context/AppContext';
 import './Posts.css';
 
 const Posts = ({ userId }) => {
-
+  const { user } = useContext(AppContext);
   const [userPosts, setUserPosts] = useState([]);
   const [render, setRender] = useState({});
 
@@ -37,9 +38,13 @@ const Posts = ({ userId }) => {
               post={post} showUpdatePostsObj={showUpdatePostsObj}
               setShowUpdatePostsObj={setShowUpdatePostsObj}
               setRender={setRender}
-            /> : <SinglePost post={post} />}
-          <DeleteUserPost postId={post.id} setRender={setRender} />
-          <UpdateUserPost postId={post.id} showUpdatePostsObj={showUpdatePostsObj} setShowUpdatePostsObj={setShowUpdatePostsObj} />
+            /> :
+            <SinglePost post={post} />}
+          {user.role === 2 &&
+            <>
+              <DeleteUserPost postId={post.id} setRender={setRender} />
+              <UpdateUserPost postId={post.id} showUpdatePostsObj={showUpdatePostsObj} setShowUpdatePostsObj={setShowUpdatePostsObj} />
+            </>}
         </div>
       )}
     </> :
